6ad3ce22416bfb30ff8a9b3911efa9c94d956871
carys <caryspritch@gmail.com>
2022-04-04 11:13:51 +0100

    heat map working Please enter the commit message for your changes. Lines starting

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,7 +1,8 @@
 import { Component, AfterViewInit } from '@angular/core';
 // import 'leaflet.heat/dist/leaflet-heat.js'
-import 'leaflet.heat'
+
 import * as L from 'leaflet';
+import 'leaflet.heat'
 
 
 @Component({
@@ -13,34 +14,22 @@ import * as L from 'leaflet';
 export class MapComponent implements AfterViewInit {
   
     private map: any;
-    private Points: any;
+    private myPoints: any;
     private heat: any = [];
-    private data: any = [[800,800],
-    [800,800],
-    [800,800],
-    [800,800],
-    [800,800],
-    [800,800],
-    [800,800],
-    [800,800],
-    [800,800],
-    [800,800],
-    [800,800],
-    [800,800],
-    [800,800],
-    [800,800],];
+    private options: any;
     private bounds: any;
     private marker: any;
     private image: any;
     private LightIcon: any;
-    private defaultHmRadius = 40;
-    private defaultHmBlur = 25;
+    private defaultHmRadius = 50;
+    private defaultHmBlur = 50;
     private gradient = {0.4: 'blue', 0.55: 'lime', 0.80: 'yellow', 1: 'red'}
   
     private initMap(): void {
       this.map = L.map('map', { crs: L.CRS.Simple });
       this.bounds = [[0,0],[1000,1000]];
       this.image = L.imageOverlay('assets/floor.jpg', this.bounds).addTo(this.map);
+    
       this.LightIcon = L.Icon.extend({
         options: {
         iconSize:[60, 55],
@@ -48,35 +37,35 @@ export class MapComponent implements AfterViewInit {
     });
       this.LightIcon = new this.LightIcon ({iconUrl: 'assets/light.jpg'});
       this.map.fitBounds(this.bounds);
+      this.myPoints = [
+        [400, 900, 8000 ]
+
+    ]
+    this.options = {
+      
+      layers: [
+      this.image
+      ],
+      radius: this.defaultHmRadius, blur: this.defaultHmBlur, gradient: this.gradient,
+      zoom: 12,
+      center: L.latLng(this.myPoints),
+      
+    };
   
-      this.marker = L.marker([800,800], {icon: this.LightIcon}).addTo(this.map);
-   
+      // this.marker = L.marker([400, 900 ], {icon: this.LightIcon}).addTo(this.map);
+
   
       ////Adding heat layer to a map
-      this.Points = [
-        [-60, 170 ],
-        [-60, 170 ],
-        [-60, 170 ],
-        [-60, 170 ],
-        [-60, 170 ],
-        [-60, 170 ],
-        [-60, 170 ],
-        [-60, 170 ],
-        [-60, 170 ],
-        
-
-    ];
   
-    this.heat = L.heatLayer([this.Points], {radius: this.defaultHmRadius, 
-      blur: this.defaultHmBlur, gradient:this.gradient }).addTo(this.map);}
-
-     
+  
+    this.heat = L.heatLayer(this.myPoints, this.options ).addTo(this.map);}
   
     constructor() { }
   
     ngAfterViewInit(): void {
       
       this.initMap();
+      return this.heat;
       
     }
   }
